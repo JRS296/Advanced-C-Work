b809cd98e6813b6ac88cbdd3ce8791584b8b50f2
JRS296 <jonathansamuel296@gmail.com>
2022-02-26 18:09:14 +0530

    Commit #11.1 - Completion of q A.c - 26.02.2022 @18:09

--- Miscellaneous Exercises/26.02.2022/a.c
+++ Miscellaneous Exercises/26.02.2022/a.c
@@ -5,6 +5,7 @@ i) Write a function to print the Account number and name of each customer with b
 below Rs. 100.
 ii) If a customer requests for withdrawal or deposit, the form contains the fields:
 Acct. no, amount, code (1 for deposit, 0 for withdrawal)
+
 Write a program to give a message, “The balance is insufficient for the specified
 withdrawal”, if on withdrawal the balance falls below Rs. 500.
 */
@@ -18,34 +19,30 @@ struct acc_holder
     long int acc_num;
     char name[30];
     int bal;
-} sbi[200] = {234, "Messi", 10000,
-              235, "Pele", 12345678,
-              236, "Ronaldo", 45,
-              237, "Maradona", 12,
-              238, "Beckenbauer", 200000};
+} sbi[100] = {234, "Messi", 10000, 235, "Pele", 12345678, 236, "Ronaldo", 45, 237, "Maradona", 12, 238, "Beckenbauer", 200000};
 
-void action(int accnum, int amount, int code)
+void transaction(int acc, int amt, int var)
 {
     int i;
-    for (i = 0; i < 200; i++)
-        if (sbi[i].acc_num == accnum)
+    for (i = 0; i < 100; i++)
+        if (sbi[i].acc_num == acc)
             break;
-    if (!code)
+    if (!var)
     {
-        if (sbi[i].bal - amount < 100)
+        if (sbi[i].bal - amt < 500)
         {
-            printf("\nThe balance is insufficient for the specified withdrawal");
+            printf("\nThe balance is insufficient for the specified withdrawal\n");
             return;
         }
         else
         {
-            sbi[i].bal -= amount;
+            sbi[i].bal -= amt;
             printf("\nYour new account balance is : %d", sbi[i].bal);
         }
     }
     else
     {
-        sbi[i].bal += amount;
+        sbi[i].bal += amt;
         printf("\nYour new account balance is : %d", sbi[i].bal);
     }
 }
@@ -53,12 +50,12 @@ void action(int accnum, int amount, int code)
 void belowhundred() //Function for Account Number of balance below 100
 {
     int i;
-    for (i = 0; i < 200; i++)
+    for (i = 0; i < 100; i++)
     {
         if (sbi[i].bal < 100 && sbi[i].bal > 0)
         {
             printf("\nName : %s", sbi[i].name);
-            printf("\nAccount Number : %d\n\n", sbi[i].acc_num);
+            printf("\nAccount Number : %d\n", sbi[i].acc_num);
         }
     }
 }
@@ -80,11 +77,8 @@ int main()
         printf("\nEnter amount to withdraw : ");
         scanf("%d", &amount);
     }
-    action(accnum, amount, code);
-    _getch();
-    system("cls");
-    printf("All members with account balance less than 100 are following : ");
-    below100();
-    _getch();
+    transaction(accnum, amount, code);
+    printf("\nAll members with account balance less than 100 are following : ");
+    belowhundred();
     return 0;
 }
