61067fb6147fb64318a9c078f63f397fa7969729
JRS296 <jonathansamuel296@gmail.com>
2022-03-07 20:17:27 +0530

    Commit #12.3 - Completion of 5.3 - 07.03.2022 @20:17

--- Lab Ass - 3/Week 5/q2.c
+++ Lab Ass - 3/Week 5/q2.c
@@ -1,3 +1,36 @@
-//Write a program to declare a dynamic pointer and allocate storage to store a 2D array of 50 x 60 of
-//data type float. Also write a code to input data to the dynamic array and output the data from the
-//dynamic array
+// Write a program to declare a dynamic pointer and allocate storage to store a 2D array of 50 x 60 of
+// data type float. Also write a code to input data to the dynamic array and output the data from the
+// dynamic array
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int row = 5, col = 6, len = 0;
+    float *arr = (float *)malloc(row * col * sizeof(float));
+    int i, j;
+
+    printf("Enter the data into the 2D array: \n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            //*(arr + i * col + j) = i + j
+            scanf("%f",(arr + i * col + j));
+        }
+            
+    }
+        
+    printf("The matrix elements are:\n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            printf("%.2f ", *(arr + i * col + j));
+        }
+        printf("\n");
+    }
+    free(arr);
+    return 0;
+}
--- Lab Ass - 3/Week 5/q3.c
+++ Lab Ass - 3/Week 5/q3.c
@@ -1,1 +1,58 @@
-//Write a program to check the given number is existing or not using pointers.
+// Write a program to check the given number is existing or not using pointers.
+
+#include <stdio.h>
+#define MAX_SIZE 100
+
+void inputArray(int *arr, int size);
+int search(int *arr, int size, int toSearch);
+
+int main()
+{
+    int size, toSearch, searchIndex;
+
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+    int *arr = (int *)malloc(size * sizeof(int));
+
+    printf("Enter elements in array: ");
+    inputArray(arr, size);
+
+    printf("Enter element to search: ");
+    scanf("%d", &toSearch);
+
+    searchIndex = search(arr, size, toSearch);
+
+    if (searchIndex == -1)
+        printf("%d does not exists in array.", toSearch);
+    else
+        printf("%d is found at %d position.", toSearch, searchIndex + 1);
+
+    return 0;
+}
+
+void inputArray(int *arr, int size)
+{
+    int *arrEnd = (arr + size - 1);
+    while (arr <= arrEnd)
+    {
+        //scanf("%f", (arr + i));
+        scanf("%d", arr++);
+    }
+}
+
+int search(int *arr, int size, int toSearch)
+{
+    int index = 0;
+    int *arrEnd = (arr + size - 1);
+    while (arr <= arrEnd && *arr != toSearch)
+    {
+        arr++;
+        index++;
+    }
+
+    // If element is found
+    if (arr <= arrEnd)
+        return index;
+
+    return -1;
+}
