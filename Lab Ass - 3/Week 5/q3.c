7c28b56e0a882b4d8b0eddc7cc6e0cc2d994af0d
JRS296 <jonathansamuel296@gmail.com>
2022-03-07 21:43:24 +0530

    Commit #12.4 - Completion of Week 5 - 07.03.2022 @21:43

--- Lab Ass - 3/Week 5/q3.c
+++ Lab Ass - 3/Week 5/q3.c
@@ -1,34 +1,7 @@
 // Write a program to check the given number is existing or not using pointers.
 
 #include <stdio.h>
-#define MAX_SIZE 100
-
-void inputArray(int *arr, int size);
-int search(int *arr, int size, int toSearch);
-
-int main()
-{
-    int size, toSearch, searchIndex;
-
-    printf("Enter size of array: ");
-    scanf("%d", &size);
-    int *arr = (int *)malloc(size * sizeof(int));
-
-    printf("Enter elements in array: ");
-    inputArray(arr, size);
-
-    printf("Enter element to search: ");
-    scanf("%d", &toSearch);
-
-    searchIndex = search(arr, size, toSearch);
-
-    if (searchIndex == -1)
-        printf("%d does not exists in array.", toSearch);
-    else
-        printf("%d is found at %d position.", toSearch, searchIndex + 1);
-
-    return 0;
-}
+#include <stdlib.h>
 
 void inputArray(int *arr, int size)
 {
@@ -55,4 +28,28 @@ int search(int *arr, int size, int toSearch)
         return index;
 
     return -1;
+}
+
+int main()
+{
+    int size, toSearch, searchIndex;
+
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+    int *arr = (int *)malloc(size * sizeof(int));
+
+    printf("Enter elements in array: ");
+    inputArray(arr, size);
+
+    printf("Enter element to search: ");
+    scanf("%d", &toSearch);
+
+    searchIndex = search(arr, size, toSearch);
+
+    if (searchIndex == -1)
+        printf("%d does not exist in array.", toSearch);
+    else
+        printf("%d is found at position %d.", toSearch, searchIndex + 1);
+
+    return 0;
 }
--- Lab Ass - 3/Week 5/q4.c
+++ Lab Ass - 3/Week 5/q4.c
@@ -1,1 +1,61 @@
-//Write a program to identify the highest number and number of times it occurred.
+// Write a program to identify the highest number and number of times it occurred.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void inputArray(int *arr, int size)
+{
+    int *arrEnd = (arr + size - 1);
+    while (arr <= arrEnd)
+    {
+        // scanf("%f", (arr + i));
+        scanf("%d", arr++);
+    }
+}
+
+int highest(int *arr, int s)
+{
+    int highest = 0;
+    for (int i = 0; i < s; i++)
+    {
+        if (arr[i] > highest)
+        {
+            highest = arr[i];
+        }
+    }
+    return highest;
+}
+
+int count(int *arr, int s, int highest)
+{
+    int count = 0;
+    for (int i = 0; i < s; i++)
+    {
+        if (arr[i] == highest)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    int size, toSearch, high, co;
+
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+    int *arr = (int *)malloc(size * sizeof(int));
+
+    printf("Enter elements in array: ");
+    inputArray(arr, size);
+
+
+    high = highest(arr, size);
+    co = count(arr, size, high);
+
+    printf("Highest number: %d\n", high);
+    printf("Count of Highest No: %d\n", co);
+
+    return 0;
+}
