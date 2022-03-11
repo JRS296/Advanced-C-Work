bd5a20cf0490715a346b31e72fa7f6afff9fad73
JRS296 <jonathansamuel296@gmail.com>
2022-03-11 23:04:55 +0530

    Commit #12.9 - Part 3 - Completion of Submission of DA - 11.03.2022 @23:04

--- Lab Ass - 3/Week 4/q3.c
+++ Lab Ass - 3/Week 4/q3.c
@@ -6,13 +6,64 @@
 
 #include <stdio.h>
 
-int input_avg()
+void input_avg()
+{
+    int arr[365];
+    printf("Enter average rainfall data for each day: ");
+    for(int i=0; i<365; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    avg_rain(arr);
+}
 
-int avg_rain()
+void avg_rain(int arr[365])
+{
+    int choice = 0;
+    int choice2 = 0, sum=0;
+    printf("Type 1 for Avg Rainfall per month (or) 2 for Avg Rainfall per year: ");
+    scanf("%d", &choice);
+    if (choice == 1)
+    {
+        printf("Enter number of month for avg rainfall: ");
+        scanf("%d", &choice2);
+        int months = {31,28,31,30,31,30,31,31,30,31,30,31};
+        int num1 = 0, num2 = 0;
+
+        for(int i=0; i<choice2-1; i++)
+        {
+            num1 += months[i];
+        }
+        num2 = num1 + months[choice2-1];
+
+        for (int i=num1-1; i<num2-1; i++)
+        {
+            sum += arr[i];
+        }
+        int avg = sum/months[choice2-1];
+        printf("Average Rainfall for month %d : %d\n",choice2-1, avg);
+    }
+    else if (choice == 2)
+    {
+        for(int i=0; i<365; i++)
+        {
+            sum += arr[i];
+        }
+        int avg = sum/365;
+        printf("Average Rainfall: %d\n", avg);
+    }
+    else{
+        printf("Invalide Input. \n");
+    }
+}
 
 int output()
+{
+    
+}
 
 int main()
 {
+    input_avg();
     return 0;
 }
